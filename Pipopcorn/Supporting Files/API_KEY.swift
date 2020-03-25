7c342e9b562087e641e869121fb5011ca9a0172a
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-03-25 17:41:18 -0300

    API_KEY gitignore test

---
+++ Pipopcorn/Supporting Files/API_KEY.swift
@@ -0,0 +1,11 @@
+//
+//  API_KEY.swift
+//  Pipopcorn
+//
+//  Created by Lucas Fernandez Nicolau on 25/03/20.
+//  Copyright © 2020 Lucas Fernandez Nicolau. All rights reserved.
+//
+
+import Foundation
+
+let API_KEY = ""
