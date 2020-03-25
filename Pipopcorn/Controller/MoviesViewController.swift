3977895fb1cb7e0fd4641e1bf040fedf8c447529
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-03-25 17:18:48 -0300

    Project structure started

---
+++ Pipopcorn/Controller/MoviesViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MoviesViewController.swift
+//  Pipopcorn
+//
+//  Created by Lucas Fernandez Nicolau on 25/03/20.
+//  Copyright © 2020 Lucas Fernandez Nicolau. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+
+}
+
+extension MoviesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension MoviesViewController: UITableViewDelegate { }
