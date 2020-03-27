b0ad390754c6b4f742b2aca8f893eb70a615d1c2
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-03-27 02:27:45 -0300

    posterImage @IBOutlet was missing. It's fixed now

--- Pipopcorn/View/MovieTableViewCell.swift
+++ Pipopcorn/View/MovieTableViewCell.swift
@@ -14,10 +14,12 @@ class MovieTableViewCell: UITableViewCell {
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         self.containerView.layer.cornerRadius = 15
+        self.containerView.clipsToBounds = true
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
